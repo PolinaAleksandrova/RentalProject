ee4c7ba7d1286ebf7dabb07ad0a66a15dca5bd3e
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2022-07-09 23:39:20 +0200

    Premises page created

---
+++ RentalProject/Areas/Admin/Controllers/PremisesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using RentalProject.Data;
+
+namespace RentalProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PremisesController : Controller
+    {
+        private ApplicationDbContext _db;
+
+
+        public PremisesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Premises.Include(c => c.PremisesTypes).Include(f => f.SpecialTag).ToList());
+        }
+    }
+}
