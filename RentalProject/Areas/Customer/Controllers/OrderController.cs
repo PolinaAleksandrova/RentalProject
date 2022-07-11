6516cc94455fdde6e484022e2230e768c9a30017
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2022-07-11 03:00:58 +0200

    Checkout view added

---
+++ RentalProject/Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RentalProject.Data;
+using RentalProject.Models;
+using RentalProject.Utility;
+
+namespace RentalProject.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class OrderController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET Checkout actioin method
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        //POST Checkout action method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Checkout(Order anOrder)
+        {
+            List<Premises> premises = HttpContext.Session.Get<List<Premises>>("premises");
+            if (premises != null)
+            {
+                foreach (var room in premises)
+                {
+                    OrderDetails orderDetails = new OrderDetails();
+                    orderDetails.PremisesId = room.Id;
+                    anOrder.OrderDetails.Add(orderDetails);
+                }
+            }
+
+            anOrder.OrderNo = GetOrderNo();
+            _db.Orders.Add(anOrder);
+            await _db.SaveChangesAsync();
+            HttpContext.Session.Set("premises", new List<Premises>());
+            return View();
+        }
+
+
+        public string GetOrderNo()
+        {
+            int rowCount = _db.Orders.ToList().Count() + 1;
+            return rowCount.ToString("000");
+        }
+    }
+}
